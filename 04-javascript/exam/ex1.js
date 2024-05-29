6c67e5ae195af4b6306fce588ee020e2d920fa78
errorose <yoonyeseul2000@gmail.com>
2024-05-29 18:55:27 +0900

    js 함수

---
+++ 04-javascript/exam/ex1.js
@@ -0,0 +1,25 @@
+// 변수 선언 실습
+
+let yeseul = {
+    name : 'yeseul',
+    age : 25,
+    girfriend : {
+        name : '성춘향',
+         age : 16
+        },
+    isMarried : true,
+    like : ['강아지', '고양이'],
+};
+
+console.log(yeseul);
+
+// 형변환 실습
+
+let mathScore = '77';
+let engScore = '88';
+let avgScore = (Number(mathScore) + Number(engScore))/2;
+
+console.log(`평균 점수는 ${avgScore} 입니다!`);
+
+
+
