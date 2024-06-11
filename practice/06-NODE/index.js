128b6d36e747f97b841210cab42a2adb2c15bb9d
“errorose” <yoonyeseul2000@gmail.com>
2024-06-12 04:33:02 +0900

    study: 서버 구축 연습

--- practice/06-NODE/index.js
+++ practice/06-NODE/index.js
@@ -1,11 +1,23 @@
-// console.log('Hello world');
+// 내가 서버 구축하기
 
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
 http.createServer(function(req, res){
-    var _url = req.url;
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.write("<h2> Hello </h2>");
-    res.end(`<p> ${_url} </p>`);
-}).listen(8000, function(){
+    var q = url.parse(req.url, true);
+    var fileName = '.' + q.pathname;
+
+    fs.readFile(fileName, function(err, data){
+        if(err){
+            res.writeHead(404, {'Content-type' : 'text.html'});
+            return res.end('File not Found');
+        }else{
+            res.writeHead(200, {'Content-type' : 'text.html'});
+            res.write(data);
+            return res.end();
+        }
+    })
+}).listen(8000, ()=>{
     console.log('8000포트에서 서버 실행중');
-});
+})
