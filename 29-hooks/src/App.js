61d784f4bf66f0b85151673b885feb1c2e41d129
errorose <yoonyeseul2000@gmail.com>
2024-08-09 17:32:15 +0900

    study: hooks - useCallback()

--- 29-hooks/src/App.js
+++ 29-hooks/src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import UseMemoEx from './components/UseMemoEx';
+import UseCallbackEx from './components/UseCallbackEx';
+import UseCallbackEx2 from './components/UseCallbackEx2';
 
 function App() {
   return (
     <div className="App">
-      <UseMemoEx />
+      {/* <UseMemoEx />
+      <hr /> */}
+
+      <UseCallbackEx />
+      <hr />
+
+      <UseCallbackEx2 postId={9}/>
     </div>
   );
 }
