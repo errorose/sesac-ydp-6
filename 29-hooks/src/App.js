11cc77edc658dd8cd7d74cef4825267274c2ab7f
errorose <yoonyeseul2000@gmail.com>
2024-08-09 16:52:26 +0900

    study: hooks - useMemo()

---
+++ 29-hooks/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UseMemoEx from './components/UseMemoEx';
+
+function App() {
+  return (
+    <div className="App">
+      <UseMemoEx />
+    </div>
+  );
+}
+
+export default App;
