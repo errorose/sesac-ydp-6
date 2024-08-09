013734a77d0dc048b80143f92f126a5a9f92028b
errorose <yoonyeseul2000@gmail.com>
2024-08-09 18:59:30 +0900

    study: hooks

--- 29-hooks/src/App.js
+++ 29-hooks/src/App.js
@@ -3,17 +3,27 @@ import './App.css';
 import UseMemoEx from './components/UseMemoEx';
 import UseCallbackEx from './components/UseCallbackEx';
 import UseCallbackEx2 from './components/UseCallbackEx2';
+import UseReducerEx from './components/UseReducerEx';
+import useTitle from './hooks/useTitle';
+import Faq from './components/Faq';
 
 function App() {
+  useTitle('React Hooks 학습중!');
   return (
     <div className="App">
-      {/* <UseMemoEx />
-      <hr /> */}
+      <UseMemoEx />
+      <hr /> 
 
       <UseCallbackEx />
       <hr />
 
       <UseCallbackEx2 postId={9}/>
+      <hr />
+
+      <UseReducerEx />
+      <hr />
+
+      <Faq />
     </div>
   );
 }
