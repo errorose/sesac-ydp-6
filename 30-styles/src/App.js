0c26af6671ac69b5670eedfaee9266ab468db7e2
errorose <yoonyeseul2000@gmail.com>
2024-08-12 18:01:39 +0900

    study: styles

---
+++ 30-styles/src/App.js
@@ -0,0 +1,23 @@
+import CssModuleComponents from './CssModuleComponents';
+import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponent';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>React Styling</h1>
+        <hr />
+
+        <CssModuleComponents />
+        <hr />
+
+        <SassComponent />
+        <hr />
+
+        <StyledComponent />
+    </div>
+  );
+}
+
+export default App;
