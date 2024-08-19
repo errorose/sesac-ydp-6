66f3d77280164e1d2681eabc493d7edd3e0c2013
errorose <yoonyeseul2000@gmail.com>
2024-08-19 16:09:06 +0900

    study: redux 전통방식

--- 32-redux/src/App2.js
+++ 32-redux/src/App2.js
@@ -6,49 +6,49 @@ function App2() {
     // Redux 상태에서 number 값을 선택.
 
     // const number = useSelector((state) => state);
-    // console.log('number >> ', number); // {number: 100}
     const number = useSelector((state) => state.number);
-    console.log('number >> ', number);
+    // console.log('number >>>>> ', number); // {number : 100}
+    console.log('number2 >>>>> ', number); // {number : 100}
 
-  return (
-    <div className="App">
-      <h1>React Redux Ex2</h1>
-      <h2>Redux 사용!</h2>
-      <h2>number: {number}</h2>
-      <Box1 />
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>React Redux Ex2</h1>
+            <h2>Redux 사용!</h2>
+            <h2>number: {number}</h2>
+            <Box1 />
+        </div>
+    );
 }
 
 // Box1 컴포넌트
 const Box1 = () => {
-  return (
-    <div className="Box">
-      <h2>Box1 :</h2>
-      <Box2 />
-    </div>
-  );
-}
+    return (
+        <div className="Box">
+            <h2>Box1</h2>
+            <Box2 />
+        </div>
+    );
+};
 
 // Box2 컴포넌트
 const Box2 = () => {
-  return (
-    <div className="Box2">
-      <h2>Box2 : </h2>
-      <Box3 />
-    </div>
-  );
-}
+    return (
+        <div className="Box2">
+            <h2>Box2 : </h2>
+            <Box3 />
+        </div>
+    );
+};
 
 // Box3 컴포넌트
 const Box3 = () => {
-  return (
-    <div className="Box3">
-      <h2>Box3 : </h2>
-      <Box4 />
-    </div>
-  );
-}
+    return (
+        <div className="Box3">
+            <h2>Box3 : </h2>
+            <Box4 />
+        </div>
+    );
+};
 
 // Box4 컴포넌트
 const Box4 = () => {
@@ -57,13 +57,17 @@ const Box4 = () => {
     // Redux 상태에서 number 값을 선택하고, 액션을 디스패치 할 준비!
     const number = useSelector((state) => state.number);
     const dispatch = useDispatch();
-  return (
-    <div className="Box4">
-      <h2>Box4 : {number}</h2>
-      <button onClick={() => dispatch({type: 'counter/PLUS'})}>Plus</button>
-      <button onClick={() => dispatch({type: 'counter/MINUS'})}>Minus</button>
-    </div>
-  );
-}
+    return (
+        <div className="Box4">
+            <h2>Box4 : {number}</h2>
+            <button onClick={() => dispatch({ type: 'counter/PLUS' })}>
+                PLUS
+            </button>
+            <button onClick={() => dispatch({ type: 'counter/MINUS' })}>
+                MINUS
+            </button>
+        </div>
+    );
+};
 
 export default App2;
