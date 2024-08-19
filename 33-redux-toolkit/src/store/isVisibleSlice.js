f0d5005ac43a04d1f22f0d6a113bc5061c148fbf
errorose <yoonyeseul2000@gmail.com>
2024-08-19 17:28:09 +0900

    study: redux toolkit

---
+++ 33-redux-toolkit/src/store/isVisibleSlice.js
@@ -0,0 +1,18 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+// 슬라이스 정의
+const isVisibleSlice = createSlice({
+    name: 'isVisible',
+    initialState: true,
+    reducers: {
+        // 상태를 반전시키는 액션
+        toggle: (state) => {
+            return !state; // 상태 반전
+        }
+    }
+});
+
+// 액션 생성자와 리듀서를 내보내기.
+export const {toggle} = isVisibleSlice.actions;
+
+export default isVisibleSlice.reducer;
