e384739533242b54c4dfd3bd2638e58404e31044
errorose <yoonyeseul2000@gmail.com>
2024-08-23 14:06:01 +0900

    study: typescript-react (todo)

--- 35-typescript-react/src/App.tsx
+++ 35-typescript-react/src/App.tsx
@@ -1,5 +1,6 @@
 import Card from './components/Card';
 import Student from './components/Student';
+import TodoList from './components/TodoList';
 
 function App() {
   // #1. 
@@ -8,11 +9,13 @@ function App() {
   }
   return (
     <div className="App">
-      <Student name="새싹" grade={3} handleClick={handleClick} />
+      {/* <Student name="새싹" grade={3} handleClick={handleClick} />
       <hr />
       <Card title="오늘 배울 것은?">
         <p>TypeScript with React</p>
       </Card>
+      <hr /> */}
+      <TodoList/>
     </div>
   );
 }
