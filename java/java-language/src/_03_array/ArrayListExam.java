0c7efa8816fe0226dd55d40ace80422b6bea0add
errorose <yoonyeseul2000@gmail.com>
2024-09-25 17:21:54 +0900

    study: 배열

---
+++ java/java-language/src/_03_array/ArrayListExam.java
@@ -0,0 +1,14 @@
+package _03_array;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ArrayListExam {
+    public static void main(String[] args) {
+        // ArrayList 실습
+        List<String> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
