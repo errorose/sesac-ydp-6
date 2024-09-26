83de863e580a064fddbe47981489edfebdaf4d99
errorose <yoonyeseul2000@gmail.com>
2024-09-27 01:40:52 +0900

    study: arrayList 실습

--- java/java-language/src/_03_array/ArrayListExam.java
+++ java/java-language/src/_03_array/ArrayListExam.java
@@ -7,10 +7,23 @@ import java.util.Scanner;
 public class ArrayListExam {
     public static void main(String[] args) {
         // ArrayList 실습
-        List<String> list = new ArrayList<>();
+        List<String> arrayList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("문자를 입력해주세요!");
+        while (true){
+            System.out.println("문자를 입력해주세요. : ");
+            String inputText = scanner.nextLine();
 
+            if(inputText.equals("exit")){
+                break;
+            }
+
+            arrayList.add(inputText);
+        }
+
+        for(String lists : arrayList){
+            System.out.println(lists);
+        }
+        scanner.close();
     }
 }
